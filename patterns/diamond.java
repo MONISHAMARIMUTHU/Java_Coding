7a45a4367416b3d943e5bfd9eee5e65f42e8072f
MONISHAMARIMUTHU <monishamarimuthu24@gmail.com>
2025-02-07 14:09:00 +0530

    Updated files

---
+++ patterns/diamond.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class diamond{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	System.out.print("Enter no:");
+	int n = sc.nextInt();
+	for (int i = 0; i < n; i++) {
+            	for (int j = 0; j < n-i-1; j++) {
+                	System.out.print("  ");
+            	}
+
+            	for (int k = 0; k < i*2+1; k++) {
+                	System.out.print("* ");
+            	}
+        		System.out.println();
+       	 }
+       	 for (int i = 1; i < n; i++) {
+           		for (int j = 0; j < i; j++) {
+                 	System.out.print("  ");
+            	}
+
+           		for (int k = 0; k < (n-i)*2-1; k++) {
+                	System.out.print("* ");
+           		 }
+        		System.out.println();
+       	 }
+    }
+
+}
